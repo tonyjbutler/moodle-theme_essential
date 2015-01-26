0053c9364375a4e3f1cd5ff18c3b4c440c294e21
Tony Butler <tonyjbutler@gmail.com>
2015-01-26 10:38:31 +0000

    theme_essential: Don't display 'Mahara' menu item to PG applicants

--- classes/core_renderer.php
+++ classes/core_renderer.php
@@ -145,6 +145,8 @@ class theme_essential_core_renderer extends core_renderer {
      * @return string $content
      */
     protected function render_custom_menu_item(custom_menu_item $menunode, $level = 0) {
+        global $USER;
+
         static $submenucount = 0;
 
         if ($menunode->has_children()) {
@@ -194,7 +196,9 @@ class theme_essential_core_renderer extends core_renderer {
                 } else {
                     $url = '#';
                 }
-                $content .= html_writer::link($url, $menunode->get_text(), array('title' => $menunode->get_title(), 'class' => $class));
+                if (!($menunode->get_text() == 'Mahara' && strpos($USER->username, '@') !== false)) {
+                    $content .= html_writer::link($url, $menunode->get_text(), array('title' => $menunode->get_title(), 'class' => $class));
+                }
             }
             $content .= html_writer::end_tag('li');
         }
