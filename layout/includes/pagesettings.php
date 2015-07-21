144e3a082333750270e06963b9d913958a9024a1
Tony Butler <tonyjbutler@gmail.com>
2015-07-21 16:31:33 +0100

    theme_essential: Display block regions in normal place for assignments

--- layout/includes/pagesettings.php
+++ layout/includes/pagesettings.php
@@ -153,7 +153,6 @@ function essential_has_footer_region() {
     switch ($PAGE->pagetype) {
         case 'admin-plugins':
         case 'course-management':
-        case 'mod-assign-view':
         case 'mod-quiz-edit':
             $hasregion = true;
             break;
